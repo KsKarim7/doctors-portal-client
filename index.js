67d7f6364bcdff21f208674c1b48d4019bcd76ad
Khaled Karim <kskarim177@gmail.com>
2022-05-15 17:24:16 +0600

    available slots for patient updated

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services')
+        const bookingCollection = client.db('doctors_portal').collection('bookings')
+
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -25,6 +27,55 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        // warning
+        // This is not the proper way to query
+        // after learning more about mongodb. use aggregate lookup, pipeline, match, group
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'March 21, 2001';
+
+            // step:1 -get all services
+            const services = await serviceCollection.find().toArray();
+
+            // step 2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step:3 for each service, f
+            services.forEach(service => {
+                // step 4:ind bookings for that service
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                // step 5: select slots for the service Bookings: 
+                const bookedSlots = serviceBookings.map(s => s.slot);
+                // step 6: select those slots that are not included in bookedSlots
+                const available = service.slots.filter(s => !bookedSlots.includes(s))
+                service.available = available;
+            })
+
+            res.send(services)
+        })
+
+        /** 
+         * API Naming Convention
+         * app.get('/booking') --it means to get all the booking of the collection or to get more than one booking or by filter
+         * app.get('/booking:id') -- means to get a specific booking by id
+         * app.post ('/booking') -- means to add a new booking
+         * app.patch ('/booking:id') -- means to update a specific booking by id
+         * app.delete ('/booking:id') -- means to delete a specific booking by id
+        */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
+
+
     }
     finally {
 
