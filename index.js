7dd24be658946e38c57e217f7afa7aadf15321eb
Khaled Karim <kskarim177@gmail.com>
2022-05-16 22:21:38 +0600

    admin routes secured

--- index.js
+++ index.js
@@ -64,14 +64,15 @@ async function run() {
             res.send({ result, token });
         })
 
-        app.get('/admin/:email', (req, res) => {
+
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
             res.send({ admin: isAdmin })
         })
 
-        app.put('/user/:admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const requester = req.decoded.email;
             const requesterAccount = await userCollection.findOne({ email: requester })
